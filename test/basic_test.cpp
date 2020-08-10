f79835c07011dcf6f17ff264360409f199a36430
Ilia Bozhinov <ammen99@gmail.com>
2020-08-10 14:41:20 +0200

    add action test

--- test/basic_test.cpp
+++ test/basic_test.cpp
@@ -1,22 +1,6 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
-#define _USE_MATH_DEFINES
-#include <cmath>
-
-#include "src/math.hpp"
-
-static finger_t finger_in_dir(double x, double y)
-{
-    return finger_t {
-        .origin = {0, 0},
-        .current = {x, y}
-    };
-}
-
-const uint32_t lu = MOVE_DIRECTION_LEFT | MOVE_DIRECTION_UP;
-const uint32_t ld = MOVE_DIRECTION_LEFT | MOVE_DIRECTION_DOWN;
-const uint32_t rd = MOVE_DIRECTION_RIGHT | MOVE_DIRECTION_DOWN;
-const uint32_t ru = MOVE_DIRECTION_RIGHT | MOVE_DIRECTION_UP;
+#include "shared.hpp"
 
 TEST_CASE("get_move_in_direction")
 {
@@ -36,14 +20,6 @@ TEST_CASE("get_incorrect_drag_distance")
         doctest::Approx(std::sqrt(2)));
     CHECK(get_incorrect_drag_distance(finger_in_dir(5, 5), MOVE_DIRECTION_LEFT)
         == doctest::Approx(5));
-}
-
-static finger_t finger_2p(double x, double y, double a, double b)
-{
-    return finger_t {
-        .origin = {x, y},
-        .current = {a, b}
-    };
 }
 
 TEST_CASE("get_pinch_scale")
@@ -83,18 +59,6 @@ TEST_CASE("get_rotation_angle")
 TEST_CASE("finger_t")
 {
     CHECK(finger_in_dir(1, 1).delta() == point_t{1, 1});
-}
-
-static void compare_point(const point_t& a, const point_t& b)
-{
-    CHECK(a.x == doctest::Approx(b.x));
-    CHECK(a.y == doctest::Approx(b.y));
-}
-
-static void compare_finger(const finger_t& a, const finger_t& b)
-{
-    compare_point(a.origin, b.origin);
-    compare_point(a.current, b.current);
 }
 
 TEST_CASE("gesture_state_t")
---
+++ test/shared.hpp
@@ -0,0 +1,36 @@
+#define _USE_MATH_DEFINES
+#include <cmath>
+#include "src/math.hpp"
+
+static finger_t finger_in_dir(double x, double y)
+{
+    return finger_t {
+        .origin = {0, 0},
+        .current = {x, y}
+    };
+}
+
+const uint32_t lu = MOVE_DIRECTION_LEFT | MOVE_DIRECTION_UP;
+const uint32_t ld = MOVE_DIRECTION_LEFT | MOVE_DIRECTION_DOWN;
+const uint32_t rd = MOVE_DIRECTION_RIGHT | MOVE_DIRECTION_DOWN;
+const uint32_t ru = MOVE_DIRECTION_RIGHT | MOVE_DIRECTION_UP;
+
+static finger_t finger_2p(double x, double y, double a, double b)
+{
+    return finger_t {
+        .origin = {x, y},
+        .current = {a, b}
+    };
+}
+
+static void compare_point(const point_t& a, const point_t& b)
+{
+    CHECK(a.x == doctest::Approx(b.x));
+    CHECK(a.y == doctest::Approx(b.y));
+}
+
+static void compare_finger(const finger_t& a, const finger_t& b)
+{
+    compare_point(a.origin, b.origin);
+    compare_point(a.current, b.current);
+}
