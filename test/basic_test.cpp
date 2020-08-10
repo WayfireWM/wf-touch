ae71089702475c3915ba967ee80a7d3f04c1400a
Ilia Bozhinov <ammen99@gmail.com>
2020-08-10 10:06:11 +0200

    add glm type

--- test/basic_test.cpp
+++ test/basic_test.cpp
@@ -1,6 +1,10 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
 
+#include "../wayfire/touch/touch.hpp"
+
+using namespace wf::touch;
+
 TEST_CASE("Math test")
 {
 }
--- wayfire/touch/touch.hpp
+++ wayfire/touch/touch.hpp
@@ -1,1 +1,11 @@
 #pragma once
+
+#include <glm/vec2.hpp>
+
+namespace wf
+{
+namespace touch
+{
+using point_t = glm::dvec2;
+}
+}
